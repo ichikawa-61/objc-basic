a3003d2bced9eee6d3efdf72a919d8394aac4042
Manami Ichikawa <mana@ms-macbook-pro.local>
2017-02-19 00:15:05 +0900

    Account Class

---
+++ account.h
@@ -0,0 +1,24 @@
+//
+//  account.h
+//  test
+//
+//  Created by Manami Ichikawa on 2/17/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#ifndef account_h
+#define account_h
+
+
+#endif /* account_h */
+
+#import <Foundation/Foundation.h>
+@interface Account : NSObject
+
+
+@property (nonatomic) NSString   *name;
+
++ (NSString *)Profile:(NSString *)Name;
+
+
+@end
