2750f5382c6972b1b393a1aa4091d599b1858344
Manami Ichikawa <mana@ms-macbook-pro.local>
2017-02-23 02:15:21 +0900

    labelに表示　02/22

--- Screen/ViewController.m
+++ Screen/ViewController.m
@@ -16,6 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //UILabelオブジェクトの生成
+    UILabel *label = [[UILabel alloc] init];
+    //UILabelはラベルを管理するクラス。画面に文字を表示させる時に使用。
+    
+    //textプロパティで文字を設定
+        label.text = NSLocalizedString(@"start Obj-C class", comment: "");
+    
+    //UILabelで高さを動的に変える。テキストを設定した後に行う。
+   [label sizeToFit];
+    
+   label.center = self.view.center;
+    [self.view addSubview:label];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
