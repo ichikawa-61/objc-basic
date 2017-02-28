df788d4d4dfb5bcbef14f28f96acef64e4ab740d
Manami Ichikawa <mana@ms-macbook-pro.local>
2017-02-28 19:36:14 +0900

    ボタンクリックでActionSheet表示2/28

--- alert/alert/ViewController.m
+++ alert/alert/ViewController.m
@@ -43,6 +43,41 @@
     NSLog(@"Cancel");
 }
 
+
+
+- (IBAction)actionSheetButtonPressed:(id)sender {
+    
+    //タイトルとメッセージ表示
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"タイトル"
+        message:@"action sheet."
+        preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    //選択肢を表示
+    UIAlertAction *firstAction = [UIAlertAction actionWithTitle:@"Twitter"
+        style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                        NSLog(@"You pressed Twitter");
+            }];
+   
+    UIAlertAction *secondAction = [UIAlertAction actionWithTitle:@"Facebook"
+        style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                        NSLog(@"You pressed Facebook");
+            }];
+    
+    UIAlertAction *thirdAction = [UIAlertAction actionWithTitle:@"Line"
+        style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                        NSLog(@"You pressed Line");
+            }];
+    
+    [alert addAction:firstAction];
+    [alert addAction:secondAction];
+    [alert addAction:thirdAction];
+    
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -52,13 +87,24 @@
     //-------------------------
     //ボタンの生成と配置
     //-------------------------
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [btn setFrame:CGRectMake(110,210,100,40)];
-    [btn setTitle:@"ボタン" forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-    [btn addTarget:self
-            action:@selector(pushButton:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button setFrame:CGRectMake(110,210,100,40)];
+    button.center = self.view.center;
+    [button setTitle:@"ボタン2" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button addTarget:self
+               action:@selector(actionSheetButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    
+//    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+//    [btn setFrame:CGRectMake(110,210,100,40)];
+//    [btn setTitle:@"ボタン" forState:UIControlStateNormal];
+//    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+//    [btn addTarget:self
+//            action:@selector(pushButton:) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:button];
 }
 
 
