6eb6c18d137885edf6069216d052eb4bc6a9eeae
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-03-07 00:13:13 +0900

    02/26 課題６Localizable.strings

--- UITableView1/UITableView1/ViewController.h
+++ UITableView1/UITableView1/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnailImageView;
 
+@property (weak, nonatomic) IBOutlet UILabel *Contents;
 
 @end
 
--- UITableView1/UITableView1/ViewController.m
+++ UITableView1/UITableView1/ViewController.m
@@ -10,13 +10,56 @@
 
 @interface ViewController ()
 
+@property(nonatomic)  NSArray *tableData;
+@property(nonatomic)  NSArray *thumbnails;
+
 @end
 
-@implementation ViewController
+@implementation ViewController{
+
+   
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //プロジェクト内のファイルにアクセスできるオブジェクトを宣言
+    NSBundle *bundle = [NSBundle mainBundle];
+    //読み込むプロパティリストのファイルパスを指定
+    NSString *path = [bundle pathForResource:@"Property List" ofType:@"plist"];
+    //プロパティリストの中身データを取得
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSArray *tableData = [dic objectForKey:@"tableData"];
+    NSArray *thumbnails = [dic objectForKey:@"thumbnails"];
+    
+    //取得できた配列データをメンバ変数に代入
+    self.tableData = tableData;
+    self.thumbnails = thumbnails;
     // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+
+//必須　セレクション内の行の数　配列を書かない時はreturn 6;など(data source プロトコル)
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+
+    return [self.tableData count];
+}
+
+//必須テーブルの行に表示する値を表示(data source プロトコル)
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [self.tableData objectAtIndex:indexPath.row];
+    //最初の行、indexPath.row propertyは０をreturn。objectAtIndex” method.を使って thumbnails array から最初のイメージを取得。
+    cell.imageView.image = [UIImage imageNamed:[self.thumbnails objectAtIndex:indexPath.row]];
+    return cell;
 }
 
 
